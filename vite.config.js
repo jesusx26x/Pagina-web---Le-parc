f0487aaf1b3f15568d0609f9092087aaa38c50aa
Jesus de los Santos <delossantosjesus26@gmail.com>
2026-03-04 08:30:23 -0400

    chore: add vite.config.js for GitHub Pages base URL

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    // Configuración de la ruta base para GitHub Pages
+    // Esto asegura que Vite construya los assets relativos a este repositorio
+    base: '/Pagina-web---Le-parc/',
+});
